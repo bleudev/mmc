58dd3358b6898514fe9da31aac13973f0f6b3df7
bleudev <aitiiigg1@gmail.com>
2025-12-23 14:19:40 +0300

    Setup auto release publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,20 @@
 import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.collections.ArrayList
 
 plugins {
     kotlin("jvm") version "2.3.0"
     id("fabric-loom") version "1.14-SNAPSHOT" apply false
     id("maven-publish")
     id("com.modrinth.minotaur") version "2.+" apply false
+    id("com.fizzpod.github-release") version "3.1.3"
 }
 
 repositories {
     mavenCentral()
 }
+
+val gversion = project.findProperty("general_version") as String
 
 fun configure(v: String, maxExv: String) {
     var changelog: String = Files.readString(Path.of(uri(project.file("CHANGELOG.md").getAbsolutePath())))
@@ -19,10 +23,24 @@ fun configure(v: String, maxExv: String) {
             set("changelog", changelog)
             set("minecraft_version", v)
             set("max_exc_version", maxExv)
-            set("mod_version", "${project.findProperty("general_version")}+$v")
+            set("mod_version", "$gversion+$v")
         }
     }
 }
 
 configure("1.21", "1.21.9")
 configure("1.21.9", "1.21.12")
+
+githubRelease {
+    token = System.getenv("GITHUB_TOKEN")
+    tagName = gversion
+    releaseName = gversion
+    generateReleaseNotes = false
+    releaseAssets = {
+        val res = ArrayList<Any>()
+        subprojects {
+            res.addAll(this.tasks.build.get().outputs.files)
+        }
+        res
+    }
+}
