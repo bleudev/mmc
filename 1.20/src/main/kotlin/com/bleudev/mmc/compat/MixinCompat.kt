a920633665d0d6a1dfc34ee534de2b603877442a
bleudev <aitiiigg1@gmail.com>
2026-05-31 16:39:45 +0300

    End of support for 1.20 + new system for dependecies in fabric mod json

--- 1.20/src/main/kotlin/com/bleudev/mmc/compat/MixinCompat.kt
+++
@@ -1,9 +0,0 @@
-package com.bleudev.mmc.compat
-
-import com.bleudev.mmc.mixin.client.accessor.ChatScreenAccessor
-import net.minecraft.client.gui.components.CommandSuggestions
-import net.minecraft.client.gui.screens.ChatScreen
-
-// Accessors
-val ChatScreen.suggestions: CommandSuggestions
-    get() = (this as ChatScreenAccessor).`mmc$commandSuggestions`()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,6 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
-include(":1.20")
 include(":1.21")
 include(":1.21.9")
 include(":26.1")
